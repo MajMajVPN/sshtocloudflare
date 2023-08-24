bd35856c027618253a9af3150432545791c4cf56
atyorkide <40038604+atyorkide@users.noreply.github.com>
2023-08-24 15:30:25 +0330

    Update sshtocloudflare.sh

--- sshtocloudflare.sh
+++ sshtocloudflare.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+
 ssh-keygen
 cat /home/$USER/.ssh/id_rsa.pub >> /home/$USER/.ssh/authorized_keys
-
 echo "public key has copied in /home/$USER/.ssh/authorized_keys"
-
 sudo cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-
 echo "public key has copied in /root/.ssh/authorized_keys"
-
 echo "ip iran ra vared konid"
 read iranip
 echo "ip kharej ra vared konid"
